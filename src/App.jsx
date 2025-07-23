a529a58577d9acff54dbf4166f5b896e4b8b5850
vanya-vb <vbozadzhieva29@gmail.com>
2025-07-23 12:04:16 +0300

    fix the navbar with a layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-import { Routes, Route, Link } from "react-router"
+import { Routes, Route } from "react-router"
 import Home from "./pages/Home"
 import About from "./pages/About"
-
-import "../server"
+import Layout from "./components/Layout"
 import Vans from "./pages/Vans"
 import VanDetails from "./pages/VanDetails"
 
+import "../server"
+
 function App() {
-
   return (
-    <>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetails />} />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   )
 }
 
