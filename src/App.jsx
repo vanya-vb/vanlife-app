0c63ef49d2bf14998dd67ed2ab3e1572ec9ebe4d
vanya-vb <vbozadzhieva29@gmail.com>
2025-07-23 12:55:19 +0300

    change absolute paths to relative

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ import "../server"
 function App() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetails />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetails />} />
 
-        <Route path="/host" element={<HostLayout />} >
+        <Route path="host" element={<HostLayout />} >
           <Route index element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
     </Routes>
